d25cd805bb5f6df5143d74a9b5ef819aaf19751e
shansoft <shansoft@5b0d8988-3928-0410-9553-cca4420a4cc9>
2008-01-07 20:59:09 +0000

    修正回收問題

--- L2JTW_GameServer_4.5_Alpha_CT1/java/net/sf/l2j/gameserver/handler/skillhandlers/Spoil.java
+++ L2JTW_GameServer_4.5_Alpha_CT1/java/net/sf/l2j/gameserver/handler/skillhandlers/Spoil.java
@@ -24,15 +24,12 @@ import net.sf.l2j.gameserver.model.L2Character;
 import net.sf.l2j.gameserver.model.L2ItemInstance;
 import net.sf.l2j.gameserver.model.L2Object;
 import net.sf.l2j.gameserver.model.L2Skill;
-import net.sf.l2j.gameserver.model.L2Summon;
 import net.sf.l2j.gameserver.model.L2Skill.SkillType;
 import net.sf.l2j.gameserver.model.actor.instance.L2MonsterInstance;
-import net.sf.l2j.gameserver.model.actor.instance.L2NpcInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.network.SystemMessageId;
 import net.sf.l2j.gameserver.serverpackets.SystemMessage;
 import net.sf.l2j.gameserver.skills.Formulas;
-
 
 /**
  * @author _drunk_
@@ -43,78 +40,70 @@ import net.sf.l2j.gameserver.skills.Formulas;
 public class Spoil implements ISkillHandler
 {
     //private static Logger _log = Logger.getLogger(Spoil.class.getName());
-
-	private static final SkillType[] SKILL_IDS = {SkillType.SPOIL, SkillType.SPOILATK};
-
+    private static final SkillType[] SKILL_IDS = {SkillType.SPOIL,SkillType.SPOILATK};
 
     public void useSkill(L2Character activeChar, L2Skill skill, @SuppressWarnings("unused") L2Object[] targets)
     {
-    	
-    	
         if (!(activeChar instanceof L2PcInstance))
-			return;
-     
-		L2Object[] targetList = skill.getTargetList(activeChar);
+            return;
 
+        L2Object[] targetList = skill.getTargetList(activeChar);
 
         if (targetList == null)
         {
             return;
         }
 
-		for (int index = 0; index < targetList.length; index++)
-		{
-			if (!(targets[index] instanceof L2MonsterInstance))
-				continue;
+        for (int index = 0; index < targetList.length; index++)
+        {
+            if (!(targetList[index] instanceof L2MonsterInstance))
+                continue;
 
-			L2MonsterInstance target = (L2MonsterInstance) targets[index];
+            L2MonsterInstance target = (L2MonsterInstance) targetList[index];
 
-			if (target.isSpoil()) {
-				activeChar.sendPacket(new SystemMessage(SystemMessageId.ALREDAY_SPOILED));
-				continue;
-			}
+            if (target.isSpoil()) {
+                activeChar.sendPacket(new SystemMessage(SystemMessageId.ALREDAY_SPOILED));
+                continue;
+            }
 
-			// SPOIL SYSTEM by Lbaldi
-			boolean spoil = false;
-			if ( target.isDead() == false ) 
-			{
-				spoil = Formulas.getInstance().calcMagicSuccess(activeChar, (L2Character)targets[index], skill);
-				int damage;
-				if (spoil)
-				{
-
-					target.setSpoil(true);
-
-					target.setIsSpoiledBy(activeChar.getObjectId());
-					activeChar.sendPacket(new SystemMessage(SystemMessageId.SPOIL_SUCCESS));
-
-
-				}
-				else 
-				{
-					SystemMessage sm = new SystemMessage(SystemMessageId.S1_WAS_UNAFFECTED_BY_S2);
-					if(target instanceof L2NpcInstance)
-					{
-						if(target instanceof L2NpcInstance)
-						{
-						if (((L2NpcInstance)target).getTemplate().serverSideName)
-						{
-							sm.addString(target.getName());
-						}
-						else
-							sm.addNpcName(((L2NpcInstance)target).getTemplate().idTemplate);
-						}
-			
-					}
-					else
-					sm.addString(target.getName());
-					sm.addSkillName(skill.getDisplayId());
-					activeChar.sendPacket(sm);
-				}				
-				target.getAI().notifyEvent(CtrlEvent.EVT_ATTACKED, activeChar);
-
-			}
-		}
+            // SPOIL SYSTEM by Lbaldi
+            // NEW SPOIL TYPE by ShanSoft
+            boolean spoil = false;
+            int damage;
+            if ( target.isDead() == false ) 
+            {
+                spoil = Formulas.getInstance().calcMagicSuccess(activeChar, (L2Character)targetList[index], skill);
+                if (skill.getSkillType()== SkillType.SPOILATK)
+                {
+                Formulas f = Formulas.getInstance();
+                L2ItemInstance weapon = activeChar.getActiveWeaponInstance();
+                boolean crit = false;
+                boolean dual = activeChar.isUsingDualWeapon();
+                boolean shld = f.calcShldUse(activeChar, target);
+                boolean soul = (weapon != null && weapon.getChargedSoulshot() == L2ItemInstance.CHARGED_SOULSHOT);
+                if (skill.getBaseCritRate() > 0)
+                    crit = f.calcCrit(skill.getBaseCritRate() * 10 * f.getSTRBonus(activeChar));
+                damage = (int) f.calcPhysDam(activeChar, target, skill, shld, false, dual, soul);
+                if (crit) damage *= 2; // PDAM Critical damage always 2x and not affected by buffs
+                activeChar.sendDamageMessage(target, damage, false, crit, false);
+                target.reduceCurrentHp(damage, activeChar);
+                }
+                if (spoil)
+                {
+                    target.setSpoil(true);
+                    target.setIsSpoiledBy(activeChar.getObjectId());
+                    activeChar.sendPacket(new SystemMessage(SystemMessageId.SPOIL_SUCCESS));
+                }
+                else 
+                {
+                    SystemMessage sm = new SystemMessage(SystemMessageId.S1_WAS_UNAFFECTED_BY_S2);
+                    sm.addString(target.getName());
+                    sm.addSkillName(skill.getDisplayId());
+                    activeChar.sendPacket(sm);
+                }               
+                target.getAI().notifyEvent(CtrlEvent.EVT_ATTACKED, activeChar);
+            }
+        }
     } 
     
     public SkillType[] getSkillIds()
