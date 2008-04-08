43a9fa423ca2e4e0c3b2f239086640072fa8d7e1
shansoft <shansoft@5b0d8988-3928-0410-9553-cca4420a4cc9>
2008-04-08 18:09:28 +0000

    修正一堆EN造成的錯誤 [sync 1933]

--- L2JTW_GameServer_4.5_Alpha_CT1/java/net/sf/l2j/gameserver/model/actor/instance/L2SiegeGuardInstance.java
+++ L2JTW_GameServer_4.5_Alpha_CT1/java/net/sf/l2j/gameserver/model/actor/instance/L2SiegeGuardInstance.java
@@ -43,10 +43,6 @@ public final class L2SiegeGuardInstance extends L2Attackable
     @SuppressWarnings("hiding")
 	private static Logger _log = Logger.getLogger(L2GuardInstance.class.getName());
 
- 	private int _homeX;
-    private int _homeY;
-    private int _homeZ;
-
     public L2SiegeGuardInstance(int objectId, L2NpcTemplate template)
     {
         super(objectId, template);
@@ -81,6 +77,9 @@ public final class L2SiegeGuardInstance extends L2Attackable
     @Override
 	public boolean isAutoAttackable(L2Character attacker)
 	{
+    	if ( attacker == null || !(attacker instanceof L2PcInstance) )
+    		return false;
+    	
         boolean isCastle = ( getCastle() != null && getCastle().getCastleId() > 0 
                              && getCastle().getSiege().getIsInProgress()
                              && !getCastle().getSiege().checkIsDefender(((L2PcInstance)attacker).getClan()));
@@ -90,9 +89,7 @@ public final class L2SiegeGuardInstance extends L2Attackable
                 && !getFort().getSiege().checkIsDefender(((L2PcInstance)attacker).getClan()));
         
         // Attackable during siege by all except defenders ( Castle or Fort )
-		return (attacker != null
-		        && attacker instanceof L2PcInstance
-		        && (isCastle || isFort) );
+		return (isCastle || isFort);
     }
 
     @Override
@@ -100,32 +97,6 @@ public final class L2SiegeGuardInstance extends L2Attackable
 	{
 		return false;
 	}
-	
-	/**
-     * Sets home location of guard. Guard will always try to return to this location after
-     * it has killed all PK's in range.
-     *
-     */
-    public void getHomeLocation()
-    {
-        _homeX = getX();
-        _homeY = getY();
-        _homeZ = getZ();
-
-        if (Config.DEBUG)
-            _log.finer(getObjectId()+": Home location set to"+
-                    " X:" + _homeX + " Y:" + _homeY + " Z:" + _homeZ);
-    }
-
-    public int getHomeX()
-    {
-    	return _homeX;
-    }
-
-    public int getHomeY()
-    {
-    	return _homeY;
-    }
     
     /**
      * This method forces guard to return to home location previously set
@@ -133,14 +104,14 @@ public final class L2SiegeGuardInstance extends L2Attackable
      */
     public void returnHome()
     {
-        if (!isInsideRadius(_homeX, _homeY, 40, false))
+        if (!isInsideRadius(getSpawn().getLocx(), getSpawn().getLocy(), 40, false))
         {
             if (Config.DEBUG) _log.fine(getObjectId()+": moving home");
             setisReturningToSpawnPoint(true);    
             clearAggroList();
             
             if (hasAI())
-                getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, new L2CharPosition(_homeX, _homeY, _homeZ, 0));
+                getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, new L2CharPosition(getSpawn().getLocx(), getSpawn().getLocy(), getSpawn().getLocz(), 0));
         }
     }
 
